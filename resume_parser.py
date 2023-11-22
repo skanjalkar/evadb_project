783fc4c883abf6a5ce7550a9c17515f56fa2619a
skanjalkar <shreyaskanjalkar@gmail.com>
2023-11-22 06:10:50 -0500

    created personal open ai function

--- resume_parser.py
+++ resume_parser.py
@@ -22,9 +22,54 @@ from evadb.functions.decorators.io_descriptors.data_types import PandasDataframe
 from evadb.functions.gpu_compatible import GPUCompatible
 from evadb.utils.generic_utils import try_to_import_ultralytics
 
+# this function takes the model and temperature as arguments from the user.
+
 class Parser(AbstractFunction):
     @property
-    def name(self) -> str:
+    def name(self):
         return "Parser"
 
-    ## TODO
+
+    @setup(cacheable=True, function_type="chat-completion", batchable=True)
+    def setup( self, model="gpt-3.5-turbo", temperature: float = 0,) -> None:
+        # assert model in _VALID_CHAT_COMPLETION_MODEL, f"Unsupported ChatGPT {model}"
+        self.model = model
+        self.temperature = temperature
+
+    @forward(
+        input_signatures=[
+            PandasDataframe(
+                columns=["query", "content", "prompt"],
+                column_types=[
+                    NdArrayType.STR,
+                    NdArrayType.STR,
+                    NdArrayType.STR,
+                ],
+                column_shapes=[(1,), (1,), (None,)],
+            )
+        ],
+        output_signatures=[
+            PandasDataframe(
+                columns=["response"],
+                column_types=[
+                    NdArrayType.STR,
+                ],
+                column_shapes=[(1,)],
+            )
+        ],
+    )
+    def forward(self, text_df):
+        import openai
+
+        #getting the data
+        content = text_df[text_df.columns[0]]
+        responses = []
+
+        for prompt in content:
+            response = openai.ChatCompletion.create(model="gpt-3.5-turbo", \
+                                                    temperature=0.2, \
+                                                    messages=[{"role": "user", "content": prompt}])
+            response_text = response.choices[0].message.content
+            responses.append(response_text)
+
+        return_df = pd.DataFrame({"response": responses})
