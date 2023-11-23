fa3fa13e2ba5ca166552745a390ebdea08e610d9
skanjalkar <shreyaskanjalkar@gmail.com>
2023-11-22 23:33:24 -0500

    added gptcache to speed up query

--- resume_evadb_project.py
+++ resume_evadb_project.py
@@ -8,6 +8,7 @@ from evadb.configuration.constants import EvaDB_INSTALLATION_DIR
 
 from utils import read_text_line, write_dict_to_files
 
+import gptcache
 
 
 PATH_TO_JOB = "JobDescription/*.pdf"
@@ -108,24 +109,34 @@ def find_match(cursor):
 
 
     completion = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
+        model="gpt-3.5-turbo-1106",
         messages=[
             {"role": "system", "content": "You will be provided with a block of text about someone's resume, and your task is to find out what the person is good at, understand the difficulty of each work they have done so far."},
             {"role": "user", "content": f"{list_string}"}
         ]
     )
     resume_summary = completion.choices[0].message
+    # store resume summary so gptcache can use it
+
     replies = {}
-    final_data_jobs = text_summarizer(cursor=cursor)    
+    final_data_jobs = text_summarizer(cursor=cursor)
     for i, val in enumerate(final_data_jobs):
+        start = time.time()
+        print("------------------------------------------------------------------------")
+        print("Starting the process of matching the resume with the job description")
+        print("------------------------------------------------------------------------")
         completion = openai.ChatCompletion.create(
-            model="gpt-3.5-turbo",
+            model="gpt-3.5-turbo-1106",
             messages=[
                 {"role": "system", "content": "You are now acting as a professional recruiter. You will be given job description and you have to summarize it so that it can be used to match with the resumes."},
                 {"role": "user", "content": f"Here is some context about the resume: {resume_summary}"},
                 {"role": "user", "content": f"Match the resume with the job description: {val} and only give me a score based on % from 0 to 100 based on how well the resume keywords matches the job description. Don't give me any extra information, only the percentage score."}
             ]
         )
+        end = time.time()
+        print("------------------------------------------------------------------------")
+        print("Time taken to match each job with resume is: ", end - start)
+        print("------------------------------------------------------------------------")
         replies[i] = completion.choices[0].message
     # print(completion.choices[0].message)
     print(replies)
